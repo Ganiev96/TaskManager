5d209023d7268996f59d33cf6e3c33d2b39ed8b4
Xayrullo <ganiev96@gmail.com>
2026-02-09 03:02:33 +0500

    Disable Swagger in production

--- TaskManager/Program.cs
+++ TaskManager/Program.cs
@@ -81,10 +81,11 @@ builder.Services.AddScoped<IProjectService, ProjectService>();
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
-
-app.UseSwagger();
-app.UseSwaggerUI();
-
+if (app.Environment.IsDevelopment())
+{
+    app.UseSwagger();
+    app.UseSwaggerUI();
+}
 
 app.UseHttpsRedirection();
 
